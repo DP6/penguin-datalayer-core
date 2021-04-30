c04a6caeeadf99ec0a1f9eac2b057908ea5d1cff
Joaquim Neto <jsn.piata@gmail.com>
2021-04-30 09:57:48 -0300

    test: :zap: Improve test to schema-parse

--- test/unit/schema-parser.test.js
+++ test/unit/schema-parser.test.js
@@ -10,20 +10,42 @@ afterEach(): It’s a hook to run after each it() or describe();
 const { assert } = require('chai');
 const chai = require('chai');
 const expect = chai.expect;
-const sinon = require('sinon');
+const fs = require('fs');
 
-const parse = require('./../../schema-parser');
-
+let parse;
+const schemaGlobal = JSON.parse(fs.readFileSync('test/unit/schema-global.json').toString());
 describe('schema-parser', () => {
+  beforeEach(() => {
+    parse = require('./../../schema-parser');
+  });
+
   describe('#parseEvent()', () => {
     it('Deve ser uma function', () => {
       assert.isFunction(parse.parseEvent);
+    });
+    it('Deve transformar uma regra de validação em um evento da camada de dados', () => {
+      let result = parse.parseEvent(schemaGlobal.array.items[0]);
+
+      expect(result).to.be.an('Object').that.not.null;
+      expect(result).to.haveOwnProperty('event');
+      expect(result.event).to.be.equal('update');
+      expect(result.aplicacao.ambiente).to.be.equal('producao');
     });
   });
 
   describe('#parseToDataLayer()', () => {
     it('Deve ser uma function', () => {
       assert.isFunction(parse.parseToDataLayer);
+    });
+    it('Deve transformar o schema de validação código de modelo da camada de dados', () => {
+      let result = [];
+      result = result.concat(parse.parseToDataLayer(schemaGlobal.array.items));
+      
+      expect(result).to.be.an('array').that.not.empty;
+      expect(result).to.be.an('array').that.have.lengthOf(4);
+      expect(result[0].event).to.be.equal('update');
+      expect(result[3].usuario.idUsuario).to.be.equal('N/A');
+      expect(result[0].aplicacao).instanceOf(Object);
     });
   });
 });
